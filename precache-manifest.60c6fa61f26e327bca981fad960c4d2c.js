46013ef7577833c1f12f345bf06f2eba59f85568
pankajcoding <pankajkumar196yahoo@gmail.com>
2019-03-05 16:56:25 +0530

    Updates

---
+++ precache-manifest.60c6fa61f26e327bca981fad960c4d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a3bfbe847c135165297",
+    "url": "/myapp/static/css/main.9966c923.chunk.css"
+  },
+  {
+    "revision": "2a3bfbe847c135165297",
+    "url": "/myapp/static/js/main.2a3bfbe8.chunk.js"
+  },
+  {
+    "revision": "569b2b33e303e431ba73",
+    "url": "/myapp/static/js/runtime~main.569b2b33.js"
+  },
+  {
+    "revision": "df484e6b08fa94170549",
+    "url": "/myapp/static/js/2.df484e6b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e3ab33725303c54d0a9cb6db6ace3ccf",
+    "url": "/myapp/index.html"
+  }
+];
